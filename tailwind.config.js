2a7fac3a71530b6e45f2b9f8dd5cf013815b319f
Maximilian.laepple <m.max32@yahoo.com>
2024-10-29 16:08:01 +0100

    Added socials, change colors, added change language

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,15 +6,26 @@ module.exports = {
             fontFamily: {
                 'bebas': ['"Bebas Neue"', 'cursive'],
                 'teko': ['"Teko"', 'sans-serif'],
-            }, colors: {
+            },
+            colors: {
                 'custom-gray': 'rgb(202, 210, 197)',  // Sanftes Grün-Grau
+                'custom-gray2': 'rgb(175,182,167)',  // Sanftes Grün-Grau 2
                 'custom-green1': 'rgb(132, 169, 140)',  // Hellgrün
                 'custom-green2': 'rgb(82, 121, 111)',   // Mittleres Grün-Blau
                 'custom-green3': 'rgb(53, 79, 82)',     // Dunkles Blau-Grau
                 'custom-green4': 'rgb(47, 62, 70)',     // Sehr dunkles Blau-Grau
             },
+            animation: {
+                'bounce-slow': 'bounce 2s infinite', // Langsamere Bounce-Animation
+                'bounce-custom': 'customBounce 1.5s infinite', // Angepasste Animation
+            },
+            keyframes: {
+                customBounce: {
+                    '0%, 100%': {transform: 'translateY(0)'},
+                    '70%': {transform: 'translateY(-15px)'}, // Höhe des Sprungs anpassen
+                },
+            },
         },
     },
     plugins: [],
 }
-
